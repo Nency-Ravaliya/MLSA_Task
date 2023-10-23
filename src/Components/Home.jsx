9efd13841bc349e6b16b0905a7685aa8f4cfe1f5
Nensi Ravaliya <117109265+Nency-Ravaliya@users.noreply.github.com>
2023-10-23 23:37:47 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -27,7 +27,7 @@ const imageAltText = "Nensi Ravaliya - Microsoft Student Ambassador.";
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="error" />
+      <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{}</h1>
         <h2>{}</h2>
