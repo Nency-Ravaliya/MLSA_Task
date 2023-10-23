820e6ce7171478005f25f87a69f4420d00083f0a
Nensi Ravaliya <117109265+Nency-Ravaliya@users.noreply.github.com>
2023-10-23 23:49:57 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/UNPLASH/wire.jpg";
+import image from "../images/UNPLASH/onelotus.jpg";
 
 const imageAltText = "Nensi Ravaliya - Microsoft Student Ambassador.";
 
