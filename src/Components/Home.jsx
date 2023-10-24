bdee7da98b4317db70e25b1ffa84ce00476ebf60
Nensi Ravaliya <117109265+Nency-Ravaliya@users.noreply.github.com>
2023-10-24 09:54:41 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -31,6 +31,7 @@ const Home = ({ name, title }) => {
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
         <h1>{}</h1>
         <h2>{}</h2>
+        <h2>{}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
